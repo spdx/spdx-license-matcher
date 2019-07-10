35b86daa8fc942bf49c3eb08e2762171c8b3570c
Ugtan <umang.taneja0173@gmail.com>
2019-07-10 22:40:22 +0530

    Use click instead of argparse + move major logic to computation file

---
+++ computation.py
@@ -0,0 +1,68 @@
+import redis
+
+from bigrams import generate_bigrams
+from normalize import normalize
+from sorensen_dice import get_dice_coefficient
+from utils import (checkTextStandardLicense, decompressBytesToString,
+                   getListedLicense)
+
+
+def get_close_matches(inputText, threshold, limit):
+    """Normalizes the given license text and forms bigrams before comparing it
+    with a database of known licenses.
+
+    Arguments:
+        text {string} -- text is the license text input by the user.
+
+    Returns:
+        dictionary -- dictionary with license name as key and dice coefficient as value.
+    """
+    normalizedInputText = normalize(inputText)
+    aBigram = generate_bigrams(normalizedInputText)
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    matches = {}
+    perfectMatches = {}
+    for key in r.keys('*'):
+        licenseName = key.decode('utf-8')
+        normalizedLicenseText = decompressBytesToString(r.get(key))
+        bBigram = generate_bigrams(normalizedLicenseText)
+        score = get_dice_coefficient(aBigram, bBigram)
+
+        if limit < score or score == 1.0:
+            perfectMatches[licenseName] = score
+        else:
+            matches[licenseName] = score
+    if perfectMatches:
+        return perfectMatches
+    matches = {licenseName: score for licenseName, score in matches.items() if threshold<score<1.0}
+    return matches
+
+
+def get_matching_string(matches, limit, inputText):
+    """Return the matching string with all of the license IDs matched with the input license text if none matches then it returns empty string.
+    
+    Arguments:
+        matches {dictionary} -- Contains the license IDs(which matched with the input text) with their respective sorensen dice score as valus.
+        limit {float} -- limit at which we will consider the match as a perfect match.
+        inputText {string} -- license text input by the user.
+    
+    Returns:
+        string -- matching string containing the license IDs that actually matched else returns empty string.
+    """
+    if not matches:
+        matchingString = 'There is not enough confidence threshold for the text to match against the SPDX License database.'
+        return matchingString
+    
+    elif 1.0 in matches.values() or all(limit < score for score in matches.values()):
+        matchingString = 'The following license ID(s) match: ' + ", ".join(matches.keys())
+        return matchingString
+    
+    else:
+        for licenseID in matches:
+            listedLicense = getListedLicense(licenseID)
+            isTextStandard = checkTextStandardLicense(listedLicense, inputText)
+            if not isTextStandard:
+                matchingString = 'The following license ID(s) match: ' + licenseID
+                return matchingString
+        else:
+            return ''
---
+++ matcher.py
@@ -0,0 +1,42 @@
+import click
+
+from computation import get_close_matches, get_matching_string
+from difference import generate_diff, get_similarity_percent
+from utils import colors, get_spdx_license_text
+
+
+@click.command()
+@click.option('--text_file', '-f', type=click.File('r'), help='File in which there is the text you want to match against the SPDX License database.'
+         'If not provided, a prompt will allow you to type the input text.')
+@click.option('--threshold', '-t', default=0.9, type = click.FloatRange(0.0, 1.0), help='Confidence threshold below which we just won"t consider it a match.', show_default=True)
+@click.option('--limit', '-l', default=0.99, type=click.FloatRange(0.9, 1.0), help='Limit at which we will consider a match as a perfect match.', show_default=True)
+def matcher(text_file, threshold, limit):
+    """SPDX License matcher to match license text against the SPDX license list using an algorithm which finds close matches. """
+    if text_file:
+        inputText = text_file.read()
+    else:
+        inputText = click.prompt('Enter a text', type=str)
+
+    inputText = bytes(inputText, 'utf-8').decode('unicode-escape')
+    matches = get_close_matches(inputText, threshold, limit)
+    matchingString = get_matching_string(matches, limit, inputText)
+    if matchingString == '':
+        licenseID = max(matches, key=matches.get)
+        spdxLicenseText = get_spdx_license_text(licenseID)
+        similarityPercent = get_similarity_percent(spdxLicenseText, inputText)
+        click.echo(colors('\nThe given license text matches {}% with that of {} based on Levenstein distance.'.format(similarityPercent, licenseID), 94))
+        differences = generate_diff(spdxLicenseText, inputText)
+        for line in differences:
+            if line[0] == '+':
+                line = colors(line, 92)
+            if line[0] == '-':
+                line = colors(line, 91)
+            if line[0] == '@':
+                line = colors(line, 90)
+            click.echo(line)
+    else:
+        click.echo(colors(matchingString, 92))
+
+
+if __name__ == "__main__":
+    matcher()
