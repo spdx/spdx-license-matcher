b256bc8158a4f4d3e0d11e64bbb59cc8679b1f3d
Ugtan <umang.taneja0173@gmail.com>
2019-07-13 18:06:42 +0530

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+
+from setuptools import find_packages, setup
+
+import spdx_license_matcher
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(_ROOT, 'README.md')) as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="spdx-license-matcher",
+    version=spdx_license_matcher.__version__,
+    description="SPDX License matcher matches the license text given by the user against the SPDX license list using an algorithm which finds close matches.",
+    long_description=LONG_DESCRIPTION,
+    author="SPDX",
+    url="https://github.com/ugtan/spdx-license-match-tool",
+    packages=find_packages(exclude=['tests*']),
+    install_requires=requirements,
+    setup_requires=['setuptools>=39.0.1'],
+    entry_points={'console_scripts': [
+        'spdx-license-matcher = spdx_license_matcher.matcher:matcher']},
+    keywords='spdx license license-matcher',
+)
